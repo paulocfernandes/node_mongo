062715fbabb62f970dd7b78bcff8eb30d86599c1
clairtonlm <clairtonlm@gmail.com>
2023-07-10 21:43:40 -0300

    conectando ao BD

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
-import express, {request, response} from 'express';
+import express from 'express';
+import mongose from 'mongose '
+import 'dotenv/config' 
 
 
 
 const server = express()
 
+const HOST = localhost
+const PORT = 5000
 
 server.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-server.listen(process.env.PORT)
+server.listen(PORT, () => {
+    console.log (`http://${HOST}:${PORT}`)
+})
+
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+
+mongose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.mg3wraz.mongodb.net/usuario?retryWrites=true&w=majority`)
+.then(() => {
+    console.log('BD conectado com sucesso')
+} )
+.catch(error => {
+    console.log ('Erro ao conectar no BD', error)
+})
